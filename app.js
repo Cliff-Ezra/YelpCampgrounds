44ac282794fa623e2b48ff6f53090c3fc8b5e98e
Cliff-Ezra <cliffezra212@gmail.com>
2022-09-18 11:46:27 +0300

    Step 4: Add review model, create, delete, and validation

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 
-//Requiring the model
+//Requiring the models
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 //Mongoose Setup: 
 main().catch((err) => console.log('MONGO CONNECTION ERROR',err));
@@ -32,7 +33,16 @@ app.engine('ejs', ejsMate);
 // Validation using Joi middleware
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
-    
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+};
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400)
@@ -41,7 +51,7 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-//Routes:
+// !CAMPGROUND ROUTES
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -71,7 +81,8 @@ app.post('/campgrounds',validateCampground, catchAsync( async (req, res, next) =
 // SHOW - details about a particular campground
 // **********************************
 app.get('/campgrounds/:id', catchAsync( async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    // console.log(campground);
     res.render('campgrounds/show', { campground });
 }));
 // *******************************************
@@ -92,11 +103,35 @@ app.put('/campgrounds/:id', validateCampground, catchAsync( async (req, res) => 
 // *******************************************
 // DELETE- removes a single campground
 // *******************************************
-app.delete('/campgrounds/:id', catchAsync( async(req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
 }));
+
+// !REVIEWS ROUTES
+// **********************************
+// CREATE - creates a campground review
+// **********************************
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+// *******************************************
+// DELETE- removes a single review
+// *******************************************
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+
 // *******************************************
 // UNKNOWN ROUTE- Unknown route Handler
 // *******************************************
@@ -113,7 +148,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-
+// SERVER
 app.listen(3000, () => {
     console.log('SERVING ON PORT 3000');
 });
